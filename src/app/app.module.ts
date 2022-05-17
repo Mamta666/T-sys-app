e3035f4bbb83c52575aa870f9a25bc362f40897f
Mamta666 <Ramtekemamta3@gmail.com>
2022-05-17 15:07:36 +0930

    added pipe ,directive and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { FooterComponent } from './footer/footer.component';
+import { HighlightDirective } from './highlight.directive';
+import { ImgurlPipe } from './imgurl.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserComponent,
+    ListUserComponent,
+    MainContainerComponent,
+    FooterComponent,
+    HighlightDirective,
+    ImgurlPipe
   ],
   imports: [
     BrowserModule
